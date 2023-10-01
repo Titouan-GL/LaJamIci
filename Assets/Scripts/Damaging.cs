3e4f94772c0c8513bb676393d1d1321ca743ead3
Verbaudet <verbaudet.domergue@gmail.com>
2023-10-01 09:34:04 +0200

    nouveaux sons
     et pas grand chose dans le code je crois on va voir le reuf :fireemoji:

--- Assets/Scripts/Damaging.cs
+++ Assets/Scripts/Damaging.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Damaging : MonoBehaviour
 {
     public float damage = 3;
+    public Break casser;
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.GetComponent<Diggable>() != null)
         {
             collision.GetComponent<Diggable>().TakeDamage(damage);
+            casser.PlayBreakSound();
         }
         gameObject.SetActive(false);
     }
